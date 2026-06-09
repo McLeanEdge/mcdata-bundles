f15fa683ef7f36b55dfbcfee88a605e3d97b5793
McLeanEdge <mcleanedge@gmail.com>
2026-06-09 15:17:12 +0000

    Fix Vercel base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-plugins: [react()],
-base: '/mcdata-bundles/'
+  plugins: [react()],
+  base: '/'
 })
